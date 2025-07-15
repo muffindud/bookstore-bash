26e5e7753bf0cf02c3a306a80b05a7ce23fd7926
muffindud <ccatlabuga2002@gmail.com>
2025-07-15 14:42:56 +0300

    add arg parse

--- bookstore.sh
+++ bookstore.sh
@@ -1,37 +1,50 @@
-lang="en"
-
-if [[ ! $1 = "--set-language" ]]; then
-    source "language/$lang.sh"
-    echo $LOG_LANGUAGE_NOT_SPECIFIED
-elif [[ -e "./language/$2.sh" ]]; then
-    lang=$2
-    source "language/$lang.sh"
-    echo $LOG_LANGUAGE_FOUND
-else
-    source "language/$lang.sh"
-    echo $LOG_LANGUAGE_NOT_FOUND
-fi
-echo $LOG_LANGUAGE_SET $lang
-
+DEFAULT_LANG="en"
 URL="https://demoqa.com/BookStore/v1/Books"
-echo $LOG_REQUEST
+RESPONSE_FILE_NAME="response.json"
 
-curl -X 'GET' $URL 2>/dev/null > response.json
+lang=""
+lang_present=1
+
+while [[ $# -gt 0 ]]; do
+    key=$1
+    case $key in
+    -l | --set-language)
+        lang=$2
+        shift
+        shift
+        ;;
+    *)
+        shift
+        ;;
+    esac
+    shift
+done
+
+if [[ -e "./language/$lang.sh" ]]; then
+    source "./language/$lang.sh"
+    echo $LOG_LANGUAGE_LOADED $lang
+else
+    source "./language/$DEFAULT_LANG.sh"
+    echo $LOG_LANGUAGE_DEFAULT
+fi
+
+echo $LOG_REQUEST
+curl -X 'GET' $URL 2>/dev/null > $RESPONSE_FILE_NAME
 echo $LOG_SAVE
 
-books_len=$(jq '.books | length' response.json)
+books_len=$(jq '.books | length' $RESPONSE_FILE_NAME)
 echo $LOG_LEN $books_len
 
 for (( i = 0; i < $books_len; i++ )); do
-    book_dir=$(jq -r ".books[${i}].isbn" response.json)
-    book_file=$(jq -r ".books[${i}].title" response.json)
-    book_desc=$(jq -r ".books[${i}].description" response.json)
-    mkdir books/$book_dir
-    echo $LOG_DIR_CREATE "books/$book_dir"
+    book_dir=$(jq -r ".books[${i}].isbn" $RESPONSE_FILE_NAME)
+    book_file=$(jq -r ".books[${i}].title" $RESPONSE_FILE_NAME)
+    book_desc=$(jq -r ".books[${i}].description" $RESPONSE_FILE_NAME)
+    mkdir "./books/$book_dir"
+    echo $LOG_DIR_CREATE "./books/$book_dir"
 
-    echo $book_desc > books/$book_dir/"$book_file"
-    echo $LOG_DESC_WRITE "books/$book_dir/\"$book_file\""
+    echo $book_desc > "./books/$book_dir/$book_file"
+    echo $LOG_DESC_WRITE "./books/$book_dir/$book_file"
 
-    chmod 444 books/$book_dir/"$book_file"
+    chmod 444 "./books/$book_dir/$book_file"
     echo $LOG_PERMISSION
 done
